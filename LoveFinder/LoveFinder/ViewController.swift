bcfb1646f90cdeb561d6b9845a0e3d0a48433954
Lyman Lai <lyman.lai@yaha.me>
2015-04-03 00:22:20 +0800

    push ChineseZodiac

--- LoveFinder/LoveFinder/ViewController.swift
+++ LoveFinder/LoveFinder/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var gender: UISegmentedControl!
+    @IBOutlet weak var birthday: UIDatePicker!
+    @IBOutlet weak var heightNumber: UISlider!
+    @IBOutlet weak var height: UILabel!
+    @IBOutlet weak var hasProperty: UISwitch!
+    @IBOutlet weak var result: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        name.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func heightChanged(sender: AnyObject) {
+        
+    }
 
+    @IBAction func okTapped(sender: AnyObject) {
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
 }
 
